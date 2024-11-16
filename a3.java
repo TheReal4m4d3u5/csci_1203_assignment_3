16a4863fe1ec16381d0bb2d619ed19663fbe2f26
Avery Jacobson <36742380+TheReal4m4d3u5@users.noreply.github.com>
2024-11-15 19:30:59 -0600

    Add files via upload

---
+++ a3.java
@@ -0,0 +1,43 @@
+/*
+						Avery Jacobson
+						     csci 1203
+						  James Polzin
+
+Assignment 3:                 Given: 06/02/14    Due: 06/06/14
+------------                                  Points: 10
+                                            Modified: 06/05/14
+Purpose: Expand an exisiting class to add more attributes and 
+         add some additional methods.
+
+    Description:
+    -----------
+      Modify the GoodDog and GoodDogTestDrive classes from 
+      pg. 82 in the text. (See also the example on pg. 62.)
+      
+      Add the following additional instance variables/attributes to 
+      the class:
+           - Name 
+           - Breed
+           - Color
+           - Age
+  
+      In your "TestDrive" code:
+        1) make three dog variables/references
+        2) create three dogs for those variables
+        3) initialize the 3 dogs
+        4) display all the attributes of all the dogs
+           and have them demonstrate their bark
+  
+      Repeat the previous 4 steps with an array representing
+      a pack of three dogs
+
+      Do not read any inputs, just produce the output. 
+
+E-mail your source code to Otto.
+* Read and follow the Email submission Guidelines on otto.*
+                         Programming -> Otto email.
+* Read and follow the Programming Guidelines posted on otto.*
+                         Programming -> Programming guidelines
+
+
+*/
